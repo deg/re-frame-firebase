a33e91982ab26bea1ac871c377694a4e6b9e0c29
Wout Neirynck <wout@d-n.be>
2018-05-23 16:06:35 +0200

    Removed whitespaces

--- src/com/degel/re_frame_firebase.cljs
+++ src/com/degel/re_frame_firebase.cljs
@@ -102,10 +102,10 @@
 ;;;                                    "https://www.googleapis.com/auth/calendar.readonly"]
 ;;;                           :custom-parameters {"login_hint" "user@example.com"}}}
 ;;;
-(re-frame/reg-fx :firebase/google-sign-in    auth/google-sign-in)
-(re-frame/reg-fx :firebase/facebook-sign-in  auth/facebook-sign-in)
-(re-frame/reg-fx :firebase/twitter-sign-in   auth/twitter-sign-in)
-(re-frame/reg-fx :firebase/github-sign-in    auth/github-sign-in)
+(re-frame/reg-fx :firebase/google-sign-in   auth/google-sign-in)
+(re-frame/reg-fx :firebase/facebook-sign-in auth/facebook-sign-in)
+(re-frame/reg-fx :firebase/twitter-sign-in  auth/twitter-sign-in)
+(re-frame/reg-fx :firebase/github-sign-in   auth/github-sign-in)
 
 ;;; Login to firebase using email/password authentication
 ;;; or registers a new user for email/password authentication.
