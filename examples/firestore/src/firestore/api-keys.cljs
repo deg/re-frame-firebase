392e3aa9dde67e2a931ee1e980d5ae5b31cf70a7
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2018-10-21 15:19:22 -0400

    Move firestore example api keys to api-keys.cljs

---
+++ examples/firestore/src/firestore/api-keys.cljs
@@ -0,0 +1,10 @@
+(ns firestore.api-keys)
+
+;; Provide your own app info
+(defonce firebase-app-info
+  {:apiKey "MY-KEY-MY-KEY-MY-KEY-MY-KEY"
+   :authDomain "my-app.firebaseapp.com"
+   :databaseURL "https://my-app.firebaseio.com"
+   :projectId "my-app"
+   :storageBucket "my-app.appspot.com"
+   :messagingSenderId "000000000000"})
