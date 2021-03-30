159a5fda36e6d043cf4b3426a1300c8a81063f36
Murtaza <murtaza52@gmail.com>
2021-03-30 10:54:11 +0300

    upgrade firebase dep

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 ;;; Author: David Goldfarb (deg@degel.com)
 ;;; Copyright (c) 2017-8, David Goldfarb
 
-(defproject com.degel/re-frame-firebase "0.9.5-SNAPSHOT"
+(defproject com.degel/re-frame-firebase "0.9.6-SNAPSHOT"
   :description "A re-frame wrapper around firebase"
   :url "https://github.com/deg/re-frame-firebase"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
-                 [cljsjs/firebase "5.7.3-1"]
+                 [cljsjs/firebase "7.5.0-0"] ;;"5.7.3-1"
                  [re-frame "0.10.6"]
                  [com.degel/iron "0.4.0"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
